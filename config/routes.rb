7c0426af2fe7e3081d79cbf17c341deaa7bf53a7
sallycai979 <sallycai979@gmail.com>
2017-01-02 13:32:41 +0000

    add following/followers

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   
   resources :users, only: [:create, :show, :edit, :update]
   resources :microposts
+  resources :relationships, only: [:create, :destroy]
 
 end
