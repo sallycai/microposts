9ffae3f639caf00ac37692310dff235bae91c980
sallycai979 <sallycai979@gmail.com>
2017-01-05 14:41:37 +0000

    Show feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,11 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+  
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
+  end
+  
   has_many :following_relationships, class_name: "Relationship",
                                      foreign_key: "follower_id",
                                      dependent: :destroy
